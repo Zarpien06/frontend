268c06696f7586cf5d97ea21fc485d464164c438
Oscar Cruz <oscarcruzsena2006@gmail.com>
2025-11-27 13:10:24 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 async function consultarAPI() {
-  const res = await fetch("TU_URL_DEL_BACKEND");
+  const res = await fetch("https://backend-5f15.onrender.com");
   const data = await res.json();
   document.getElementById("respuesta").innerText = data.message;
 }
